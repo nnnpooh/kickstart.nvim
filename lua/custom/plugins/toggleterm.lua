a475fa5629db7199d6ca10fd48557117f4b89cef
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-04-09 09:53:13 +0700

    Update

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -40,7 +40,8 @@ return {
         persist_size = true,
         direction = 'float', -- | 'horizontal' | 'window' | 'float',
         close_on_exit = true, -- close the terminal window when the process exits
-        shell = vim.o.shell, -- change the default shell
+        -- shell = vim.o.shell, -- change the default shell
+        shell = 'powershell.exe',
         -- This field is only relevant if direction is set to 'float'
         float_opts = {
           -- The border key is *almost* the same as 'nvim_win_open'
