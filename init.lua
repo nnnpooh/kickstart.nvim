e0e18e54196f49f7efe47328d329dc3c861c37ac
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-05-16 15:50:35 +0700

    Update

--- init.lua
+++ init.lua
@@ -648,6 +648,7 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
       -- Snippet Engine & its associated nvim-cmp source
       {
         'L3MON4D3/LuaSnip',
@@ -677,7 +678,7 @@ require('lazy').setup({
       -- Adds other completion capabilities.
       --  nvim-cmp does not ship with all sources by default. They are split
       --  into multiple repos for maintenance purposes.
-      'hrsh7th/cmp-nvim-lsp',
+      -- 'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
     },
     config = function()
